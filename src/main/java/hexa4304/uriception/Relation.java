2d7697c79e82c856f611cdbff8d24048aeaef740
nsorin <nicolas.sorin1996@gmail.com>
2016-11-09 08:27:40 +0100

    Ajout IHM avec exemple

---
+++ src/main/java/hexa4304/uriception/Relation.java
@@ -0,0 +1,54 @@
+package hexa4304.uriception;
+
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class Relation extends Parent {
+
+    private Node start;
+
+    private Node end;
+
+    private double xStart;
+    private double yStart;
+    private double xEnd;
+    private double yEnd;
+
+    private String text;
+
+    public Relation() {
+        this.xStart = 0;
+        this.xEnd = 0;
+        this.yStart = 0;
+        this.yEnd = 0;
+    }
+
+    public Relation(Node s, Node e) {
+        this.start = s;
+        this.end = e;
+
+        this.text = e.getRelation();
+
+        xStart = s.getX();
+        xEnd = e.getX();
+        yStart = s.getY();
+        yEnd = e.getY();
+
+        double xText = xStart + (xStart - xEnd)/2;
+        double yText = yStart + (yStart - yEnd)/2;
+
+        Line l = new Line(this.xStart, this.yStart, this.xEnd, this.yEnd);
+        this.getChildren().add(l);
+
+        Text t = new Text(this.text);
+        t.setX(xText);
+        t.setY(yText);
+        t.setFont(new Font(10));
+        t.setFill(Color.BLUE);
+        t.setRotate(Math.toDegrees(Math.atan((yStart - yEnd)/(xStart - xEnd))));
+        this.getChildren().add(t);
+    }
+}
